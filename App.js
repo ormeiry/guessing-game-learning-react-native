e8fce7851f7a439e11a502bf5e34b642e73c1880
Or Meiry <ormeiry2411@gmail.com>
2020-10-22 17:30:53 +0300

    Added custom fonts to project and logic for loading them when app is started.

--- App.js
+++ App.js
@@ -1,16 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
+import React, { useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
 
-import Header from './components/Header'
-import StartGameScreen from './screens/StartGameScreen'
-import GameScreen from './screens/GameScreen'
-import GameOver from './screens/GameOver'
+import Header from './components/Header';
+import StartGameScreen from './screens/StartGameScreen';
+import GameScreen from './screens/GameScreen';
+import GameOver from './screens/GameOver';
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  })
+}
 
 export default function App() {
   
   const [userNumber, setUserNumber] = useState();
   const [numOfguesses, setNumOfGuesses] = useState(0);
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  if(!dataLoaded) {
+    return <AppLoading startAsync={fetchFonts} onFinish={() => setDataLoaded(true)} onError={() => console.log(err)}/>
+  }
 
   const configNewGameHandler = () => {
     setNumOfGuesses(0)
