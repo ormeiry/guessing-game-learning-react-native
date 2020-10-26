87e2dbea670e57c699de4a408c834b39bd31809e
Or Meiry <ormeiry2411@gmail.com>
2020-10-26 18:55:59 +0200

    Added a Flatlist to render the guesses in real time.

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,10 +1,11 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { View, StyleSheet, Button, Alert } from 'react-native';
+import { View, StyleSheet, Button, Alert, FlatList } from 'react-native';
 
 import NumberContainer from '../components/NumberContainer';
 import Card from '../components/Card';
 import Colors from '../constants/colors' 
 import TitleText from '../components/TitleText';
+import BodyText from '../components/BodyText';
 
 const generateRandomBetween = (min, max, exclude) => {
     min = Math.ceil(min);
@@ -17,10 +18,21 @@ const generateRandomBetween = (min, max, exclude) => {
     }
 };
 
-const GameScreen = (props) => {
+const renderListItem = (listLength, itemData) => {
+    return (
+        <View style={styles.listItem}>
+            <BodyText style={styles.guessStyle}>{itemData.item}</BodyText>
+            <View>
+                <BodyText style={styles.numStyle}>#{listLength - itemData.index}</BodyText>
+            </View>
+        </View>
+    )
+}
 
-    const [currentGuess, setCurrentGuess] = useState(generateRandomBetween(1, 100, props.userChoice));
-    const [rounds, setRounds] = useState(0);
+const GameScreen = (props) => {
+    const initialGuess = generateRandomBetween(1, 100, props.userChoice)
+    const [currentGuess, setCurrentGuess] = useState(initialGuess);
+    const [pastGuesses, setPastGuesses] = useState([initialGuess.toString()]);
     const currentLow = useRef(1)
     const currentHigh = useRef(100)
 
@@ -28,7 +40,7 @@ const GameScreen = (props) => {
     const { userChoice, onGameOver } = props;
     useEffect(() => {
         if ( currentGuess === userChoice ) {
-            props.onGameOver(rounds)
+            props.onGameOver(pastGuesses.length)
         }
     }, [currentGuess, userChoice, onGameOver])
 
@@ -38,13 +50,13 @@ const GameScreen = (props) => {
             return;
         }
         if (direction === 'lower') {
-            currentHigh.current = currentGuess - 1
+            currentHigh.current = currentGuess
         } else { 
             currentLow.current = currentGuess + 1
         }
         const nextNumber = generateRandomBetween(currentLow.current, currentHigh.current, currentGuess)
         setCurrentGuess(nextNumber)
-        setRounds(prevRound => prevRound + 1)
+        setPastGuesses(curPastGuesses => [nextNumber.toString(), ...curPastGuesses])
     };
 
     return (
@@ -52,9 +64,12 @@ const GameScreen = (props) => {
             <TitleText style={styles.guessText}>Opponent's Guess</TitleText>
             <NumberContainer>{currentGuess}</NumberContainer>
             <Card style={styles.buttonContainer}>
-                <Button color={Colors.accent} title="Lower" onPress={nextGuessHandler.bind(this, 'lower')}/>
-                <Button color={Colors.accent} title="Greater" onPress={nextGuessHandler.bind(this, 'greater')}/>
+                <Button color={Colors.accent} title="Lower" onPress={() => nextGuessHandler("lower")}/>
+                <Button color={Colors.accent} title="Greater" onPress={() => nextGuessHandler('greater')}/>
             </Card>
+            <View style={styles.listContainer}>
+                <FlatList keyExtractor={item => item} data={pastGuesses} renderItem={renderListItem.bind(this,pastGuesses.length)} contentContainerStyle={styles.list}/>
+            </View>
         </View>
     )
 }
@@ -78,6 +93,42 @@ const styles = StyleSheet.create({
     guessText: {
         fontSize: 25,
         marginBottom: 30 
+    },
+    listContainer: {
+        flex: 1,
+        width: '60%'
+    },
+    list: {
+        flexGrow: 1,
+        justifyContent: 'flex-end',
+    },
+    listItem: {
+        borderColor: Colors.accent,
+        borderWidth: 1,
+        padding: 15,
+        marginVertical: 10,
+        backgroundColor: Colors.primary,
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        width: '100%',
+        borderRadius: 10,
+        alignItems: 'center',
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2},
+        shadowRadius: 8,
+        shadowOpacity: 0.26,
+        elevation: 5,
+    },
+    numStyle: {
+        backgroundColor: 'white',
+        borderRadius: 5,
+        padding: 5
+    },
+    guessStyle: {
+        backgroundColor: Colors.accent,
+        borderRadius: 5,
+        padding: 5,
+        color: 'white'
     }
 });
 
